56ed09a4c6b55da5fbcc71e9b55d3f927b7cc7ca
Jomer.Dev <Jomer.Dev@posteo.de>
2024-04-19 18:42:33 +0200

    Add run from ram

--- eeprom/build.rs
+++ eeprom/build.rs
@@ -21,6 +21,10 @@ fn main() {
         .unwrap()
         .write_all(include_bytes!("memory.x"))
         .unwrap();
+    File::create(out.join("link.x"))
+        .unwrap()
+        .write_all(include_bytes!("link-ram.x"))
+        .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
 
     // By default, Cargo will re-run a build script whenever
@@ -28,9 +32,10 @@ fn main() {
     // here, we ensure the build script is only re-run when
     // `memory.x` is changed.
     println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rerun-if-changed=link-ram.x");
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
-    println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
+    // println!("cargo:rustc-link-arg-bins=-Tlink-rp.x"); // Not needed with running in RAM
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
 }
